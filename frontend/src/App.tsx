8e39f9fd4cded0808dcdece6168d0aa59143a687
Kristina <kristina.marienfeld@gmail.com>
2023-06-12 23:04:41 +0200

    add UseLogin in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import './App.css';
 import {Route, Routes} from "react-router-dom";
 import Login from "./login/Login";
+import UseLogin from "./login/UseLogin";
 
 function App() {
+    const {login, user} = UseLogin()
+
   return (
     <div>
         <Routes>
-        <Route path={"/login"} element={<Login/>}/>
+        <Route path={"/login"} element={<Login login={login}/>}/>
         </Routes>
     </div>
   );
