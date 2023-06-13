ff3ce61097eb37ff9006fe4e5f7341506c725619
Kristina <kristina.marienfeld@gmail.com>
2023-06-13 22:20:33 +0200

    add Header.tsx and Header.css

--- frontend/src/header/Header.tsx
+++ frontend/src/header/Header.tsx
@@ -1,19 +1,28 @@
 import React from 'react';
-import {Container, Navbar} from 'react-bootstrap'
+import {Container, Nav, Navbar, NavDropdown} from 'react-bootstrap'
 import logo from "../logo_tuerkis.png"
+import "./Header.css"
 function Header() {
     return (
         <header>
-            <Navbar bg="dark" variant="dark" fixed="top">
+            <Navbar bg="dark" variant="dark" sticky="top" expand="sm" collapseOnSelect >
                 <Container>
                     <Navbar.Brand>
                         <img
                             alt=""
                             src={logo}
                             className="d-inline-block align-top"
-                        />{' '}
-                        Logo
+                            width="100px"
+                            height="auto"
+                        />
                     </Navbar.Brand>
+                    <Navbar.Toggle/>
+                        <Navbar.Collapse>
+                            <Nav>
+                                <Nav.Link className="menuLink" href="/all-bills">Nebenostenabrechnung</Nav.Link>
+                                <Nav.Link className="menuLink" href="/all-expense-category">Kostenarten</Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
                 </Container>
             </Navbar>
         </header>
