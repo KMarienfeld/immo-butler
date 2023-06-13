7a69ff091ac063afd1742735791a7d652c6b3215
Kristina <kristina.marienfeld@gmail.com>
2023-06-13 16:31:13 +0200

    add changes

--- frontend/src/header/Header.tsx
+++ frontend/src/header/Header.tsx
@@ -4,15 +4,15 @@ import logo from "../logo_tuerkis.png"
 function Header() {
     return (
         <header>
-            <Navbar bg="dark" variant="dark">
+            <Navbar bg="dark" variant="dark" fixed="top">
                 <Container>
-                    <Navbar.Brand href="#home">
+                    <Navbar.Brand>
                         <img
                             alt=""
                             src={logo}
                             className="d-inline-block align-top"
                         />{' '}
-                        React Bootstrap
+                        Logo
                     </Navbar.Brand>
                 </Container>
             </Navbar>
