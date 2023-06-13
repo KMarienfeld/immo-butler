63f8657c8a5069d842e9505dc91bc7b118d2dc22
Kristina <kristina.marienfeld@gmail.com>
2023-06-13 22:33:40 +0200

    add header to protected Routes in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import {Route, Routes} from "react-router-dom";
 import Login from "./login/Login";
 import UseLogin from "./login/UseLogin";
 import Header from "./header/Header";
+import ProtectedRoutes from "./login/ProtectedRoutes";
 
 function App() {
     const {login, user} = UseLogin()
@@ -11,10 +12,11 @@ function App() {
   return (
     <div>
         <Routes>
-        <Route path={"/login"} element={<Login login={login}/>}/>
-
-                <Route path={"/"} element={<Header/>}/>
-
+            <Route path={"/login"} element={<Login login={login}/>}/>
+            <Route element={<ProtectedRoutes user={user}/>}>
+                <Route path={"/all-bills"} element={<Header/>}/>
+                <Route path={"/all-expense-category"} element={<Header/>}/>
+            </Route>
         </Routes>
     </div>
   );
