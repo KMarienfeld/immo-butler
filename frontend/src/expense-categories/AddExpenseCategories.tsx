0f77650154878b3cce818e0fdf7c9f08acd43ad5
Kristina <kristina.marienfeld@gmail.com>
2023-06-14 17:08:10 +0200

    add onChange and axios.put

--- frontend/src/expense-categories/AddExpenseCategories.tsx
+++ frontend/src/expense-categories/AddExpenseCategories.tsx
@@ -1,13 +1,54 @@
-import React from 'react';
+import React, {ChangeEvent, FormEvent, useState} from 'react';
 import {Button, Col, Container, Form, Row} from "react-bootstrap";
 import "./AddExpenseCategories.css"
 import {useNavigate} from "react-router-dom";
+import {ExpenseCategoryModel} from "../model/ExpenseCategoryModel";
+import axios from "axios";
+
 function AddExpenseCategories() {
 
     const navigate = useNavigate();
+    const [expanseCategoryN, setExpanseCategoryN] = useState<string>("")
+    const [distributionKeyN, setDistributionKeyN] = useState<string>("")
+    const [totalN, setTotalN] = useState<number>(0)
+    const [portionN, setPortionN] = useState<number>(0)
 
-    function onClickBackButton() {
+    function onClickGoBack() {
         navigate("/all-expense-categories")
+    }
+
+    function onChangeHandlerExpenseCategory(e:ChangeEvent<HTMLInputElement>) {
+        setExpanseCategoryN(e.target.value)
+    }
+
+    function onChangeHandlerDistributionKey(e:ChangeEvent<HTMLSelectElement>) {
+        setDistributionKeyN(e.target.value)
+    }
+
+    function onChangeHandlerTotal(e:ChangeEvent<HTMLInputElement>) {
+        const value = e.target.value;
+        setTotalN(Number(value))
+    }
+
+
+    function onChangeHandlerPortion(e:ChangeEvent<HTMLInputElement>) {
+        const value = e.target.value;
+        setPortionN(Number(value))
+    }
+
+    function addNewExpenseCategory(e:FormEvent<HTMLFormElement>) {
+        e.preventDefault()
+        const newExpenseCategory:ExpenseCategoryModel = {
+            expanseCategory:expanseCategoryN,
+            distributionKey:distributionKeyN,
+            total:totalN, portion:portionN
+        }
+       axios.post('api/expenseCategory/add', newExpenseCategory)
+           .then(response => {
+               console.log(response.data)
+           })
+           .then(()=> navigate("/all-expense-categories"))
+           .catch(error => console.log(error))
     }
 
     return (
@@ -18,14 +59,14 @@ function AddExpenseCategories() {
                 </Container>
             </Row>
             <Container className="mt-5">
-                <Form>
+                <Form onSubmit={addNewExpenseCategory}>
                     <Form.Group className="mb-3" controlId="formGridExpenseCategory">
                         <Form.Label>Kostenart</Form.Label>
-                        <Form.Control placeholder="Trage hier die Kostenart ein" />
+                        <Form.Control placeholder="Trage hier die Kostenart ein" onChange={onChangeHandlerExpenseCategory}/>
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formGridDistributionKey">
                         <Form.Label>Umlageschlüssel</Form.Label>
-                        <Form.Select defaultValue="Wähle hier einen Umlageschlüssel aus...">
+                        <Form.Select defaultValue="Wähle hier einen Umlageschlüssel aus..." onChange={onChangeHandlerDistributionKey}>
                             <option disabled>Wähle hier einen Umlageschlüssel aus...</option>
                             <option>Wohnfläche</option>
                             <option>Wohneinheiten</option>
@@ -37,16 +78,16 @@ function AddExpenseCategories() {
                     <Row className="mb-3">
                         <Form.Group as={Col} controlId="formGridTotal">
                             <Form.Label>Gesamt</Form.Label>
-                            <Form.Control placeholder="Referenz" />
+                            <Form.Control placeholder="Referenz" onChange={onChangeHandlerTotal}/>
                         </Form.Group>
                         <Form.Group as={Col} controlId="formGridPortion">
                             <Form.Label>Anteil</Form.Label>
-                            <Form.Control placeholder="Wohneinheitsanteil" />
+                            <Form.Control placeholder="Wohneinheitsanteil" onChange={onChangeHandlerPortion} />
                         </Form.Group>
                     </Row>
                     <Row className="mt-5">
                         <Col>
-                            <Button className="buttonBack" variant="outline-dark" type="submit" onClick={onClickBackButton}>
+                            <Button className="buttonBack" variant="outline-dark" type="submit" onClick={onClickGoBack}>
                                 zurück
                             </Button>
                         </Col>
