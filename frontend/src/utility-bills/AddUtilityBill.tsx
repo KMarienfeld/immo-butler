67403c81b246f27a1a6c8d77aaa887f89dbfa95b
Kristina <kristina.marienfeld@gmail.com>
2023-06-23 20:14:28 +0200

    adding some code in AddUtilityBill and ExpenseCategoryForBillFormCard

--- frontend/src/utility-bills/AddUtilityBill.tsx
+++ frontend/src/utility-bills/AddUtilityBill.tsx
@@ -1,8 +1,48 @@
 import React from 'react';
+import {Button, Col, Container, Form, Row} from "react-bootstrap";
 
 function AddUtilityBill() {
+
     return (
-        <div></div>
+        <div>
+            <Row className="mt-5">
+                <Container className="d-flex justify-content-center">
+                    <h3 className="text-center">Lege hier eine neue Nebenkostenabrechnung an:</h3>
+                </Container>
+            </Row>
+            <Container className="mt-5">
+                <Form onSubmit={addNewUtilityBill}>
+                    <Row className="mb-3">
+                        <Form.Group as={Col} controlId="formGridTotal">
+                            <Form.Label>Jahr der Abrechnung:</Form.Label>
+                            <Form.Control placeholder="Trage hier das Jahr der Abrechnung ein, z.B. 2022"
+                                          onChange={onChangeHandlerYear} pattern="[0-9]{4}"
+                                          title="Bitte geben das Kalenderjahr ein"/>
+                        </Form.Group>
+                        <Form.Group as={Col} controlId="formGridPortion">
+                            <Form.Label>monatliche Vorauszahlung:</Form.Label>
+                            <Form.Control placeholder="geleistete Vorauszahlung in Monaten"
+                                          onChange={onChangeHandlerPrepaymentMonthly} pattern="[0-9]*"
+                                          title="Bitte geben Sie nur ganze Zahlen ein"/>
+                        </Form.Group>
+                    </Row>
+                    <Row className="mt-5">
+                        <Col>
+                            <Button className="buttonBack" variant="outline-dark" onClick={onClickGoBack}>
+                                zurück
+                            </Button>
+                        </Col>
+                        <Col>
+                            <Button className="buttonSubmit" type="submit">
+                                Nebenkostenabrechnung erstellen
+                            </Button>
+                        </Col>
+                    </Row>
+                </Form>
+            </Container>
+
+
+        </div>
     );
 }
 
---
+++ frontend/src/utility-bills/ExpenseCategoryForBillFormCard.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import {Button, Col, Container, Form, OverlayTrigger, Row} from "react-bootstrap";
+import {QuestionCircleFill} from "react-bootstrap-icons";
+import {ExpenseCategoryModel} from "../model/ExpenseCategoryModel";
+
+type Props = {
+    listOfExpenseCategories: ExpenseCategoryModel[],
+}
+
+function ExpenseCategoryForBillFormCard(props: Props) {
+    return (
+        <div>
+            <Container className="mt-5">
+                <Row className="mb-3">
+                    <Form.Group as={Col} className="mb-3" controlId="formGridSelectExpenseCategory">
+                        <Form.Label>Kostenart
+                            <OverlayTrigger trigger={['hover', 'click']}
+                                            overlay={infoContentExpenseCategoryForBillFormCard}>
+                                <div><QuestionCircleFill className="question-icon"/></div>
+                            </OverlayTrigger>
+                        </Form.Label>
+                        <Form.Select defaultValue="Wähle hier eine Kostenart aus..."
+                                     onChange={onChangeHandlerExpenseCategory}>
+                            <option disabled>Wähle hier eine Kostenart aus...</option>
+                            <option value="AREABASEDKEY">Wohnfläche</option>
+                            <option value="PERSONBASEDKEY">Personenzahl</option>
+                            <option value="CONSUMPTIONBASEDKEY">Direktzuordnung</option>
+                            <option value="UNITBASEDKEY">Wohneinheiten</option>
+                        </Form.Select>
+                    </Form.Group>
+                    <Form.Group as={Col} controlId="formGridTotalBill">
+                        <Form.Label>Jahresbeitrag</Form.Label>
+                        <Form.Control placeholder="Trage hier den Jahresbeitrag ein" onChange={onChangeHandlerTotalBill}
+                                      pattern="[0-9]*([.,][0-9]+)?"
+                                      title="Es können nur Zahlen eintragen"/>
+                    </Form.Group>
+                </Row>
+                <Row className="mt-5">
+                    <Col>
+                        <Button className="buttonDeleteExpenseCategoryForBillFormCard" variant="outline-dark"
+                                onClick={onClickDeleteActualExpenseCategoryForBillFormCard}>
+                            zurück
+                        </Button>
+                    </Col>
+                    <Col>
+                        <Button className="buttonNextExpenseCategoryForBillFormCard"
+                                onClick={onClickAddNextExpenseCategoryForBillFormCard}>
+                            +
+                        </Button>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    );
+}
+
+export default ExpenseCategoryForBillFormCard;
