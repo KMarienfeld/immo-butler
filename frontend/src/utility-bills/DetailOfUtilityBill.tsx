105462d59f81c7519fadc064bebdac1140e11d05
Kristina <kristina.marienfeld@gmail.com>
2023-06-28 11:25:50 +0200

    add some visual things

--- frontend/src/utility-bills/DetailOfUtilityBill.tsx
+++ frontend/src/utility-bills/DetailOfUtilityBill.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Table} from "react-bootstrap";
+import {Col, Container, Row, Table} from "react-bootstrap";
 import {UtilityBillModel} from "../model/UtilityBillModel";
 import {useParams} from "react-router-dom";
 import {CustomExpenseCategoryForBillModel} from "../model/CustomExpenseCategoryForBillModel";
@@ -21,46 +21,58 @@ function DetailOfUtilityBill(props: Props) {
 
     return (
         <div>
-            <h1>Nebenkostenabrechnung {actualUtilityBill?.year}</h1>
-            <Table striped bordered hover size="sm">
-                <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Kostenart</th>
-                    <th>Gesamtkosten</th>
-                    <th>anteilige Kosten</th>
-                </tr>
-                </thead>
-                <tbody>
-                {listOfActualCustomExpenseCategories?.map((currentCustomExpenseCategory, index) => (
+            <Container className="d-flex justify-content-center mt-3 mb-3">
+                <h1>Nebenkostenabrechnung {actualUtilityBill?.year}</h1>
+            </Container>
+            <Container>
+                <Table striped bordered hover size="sm">
+                    <thead>
                     <tr>
-                        <td key={currentCustomExpenseCategory.id}>{index + 1}</td>
-                        <td>{currentCustomExpenseCategory.expenseCategory}</td>
-                        <td>{currentCustomExpenseCategory.totalBill}</td>
-                        <td>{currentCustomExpenseCategory.proportionalBill}</td>
+                        <th>#</th>
+                        <th>Kostenart</th>
+                        <th>Gesamtkosten</th>
+                        <th>anteilige Kosten</th>
                     </tr>
-                ))}
-                <tr>
-                    <td colSpan={3}>Gesamtbetrag: {actualUtilityBill?.totalCostsOfAllExpenseCategories}</td>
-                    <td>@twitter</td>
-                </tr>
-                </tbody>
-            </Table>
-            <p>Die geleistete Vorauszahlung pro Monat betrug: {actualUtilityBill?.prepaymentMonthly},<br/>
-                das entspricht einer Jahreszahlung von {actualUtilityBill?.prepaymentYear}.</p>
-            <p>
-                Daraus ergibt sich eine{' '}
-                {actualUtilityBill?.finalResult !== undefined
-                    ? (actualUtilityBill?.finalResult < 0 ? (
-                            <>Rückerstattung in Höhe von {actualUtilityBill?.finalResult}</>
+                    </thead>
+                    <tbody>
+                    {listOfActualCustomExpenseCategories?.map((currentCustomExpenseCategory, index) => (
+                        <tr>
+                            <td key={currentCustomExpenseCategory.id}>{index + 1}</td>
+                            <td>{currentCustomExpenseCategory.expenseCategory}</td>
+                            <td>{currentCustomExpenseCategory.totalBill}</td>
+                            <td>{currentCustomExpenseCategory.proportionalBill}</td>
+                        </tr>
+                    ))}
+                    <tr>
+                        <td colSpan={3}><strong>Gesamtbetrag: </strong></td>
+                        <td><strong>{actualUtilityBill?.totalCostsOfAllExpenseCategories}</strong></td>
+                    </tr>
+                    </tbody>
+                </Table>
+            </Container>
+            <Container className="d-flex justify-content-center">
+                <Row>
+                    <Col>Die geleistete Vorauszahlung pro Monat betrug <br
+                        className="d-sm-none"/>{actualUtilityBill?.prepaymentMonthly}€, <br className="d-sm-none"/>
+                        das entspricht einer Jahreszahlung von <br
+                            className="d-sm-none"/> {actualUtilityBill?.prepaymentYear}€.</Col>
+
+                </Row>
+            </Container>
+            <Container className="d-flex justify-content-center mt-3">
+                <p>
+                    Daraus ergibt sich eine{' '}
+                    {actualUtilityBill?.finalResult !== undefined
+                        ? (actualUtilityBill?.finalResult < 0 ? (
+                                <>Rückerstattung in Höhe von {Math.abs(actualUtilityBill?.finalResult)}€.</>
+                            ) : (
+                                <>Nachzahlung in Höhe von {actualUtilityBill?.finalResult}€.</>
+                            )
                         ) : (
-                            <>Nachzahlung in Höhe von {actualUtilityBill?.finalResult}</>
-                        )
-                    ) : (
-                        ''
-                    )}
-            </p>
-            );
+                            ''
+                        )}
+                </p>
+            </Container>
 
 
         </div>
