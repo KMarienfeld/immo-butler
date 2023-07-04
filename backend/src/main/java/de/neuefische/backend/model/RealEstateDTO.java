5d3faed2d1e86eb0524953f88d2257eee73f4dab
Kristina Marienfeld <130476631+KMarienfeld@users.noreply.github.com>
2023-07-04 19:16:31 +0200

    add List of ExpenseCategory in model

--- backend/src/main/java/de/neuefische/backend/model/RealEstateDTO.java
+++ backend/src/main/java/de/neuefische/backend/model/RealEstateDTO.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -16,4 +18,5 @@ public class RealEstateDTO {
     private GenderOfTenant genderOfTenant;
     private String firstNameOfTenant;
     private String lastNameOfTenant;
+    private List<ExpenseCategory> listOfExpanseCategories;
 }
--- backend/src/main/java/de/neuefische/backend/model/RealEstateModel.java
+++ backend/src/main/java/de/neuefische/backend/model/RealEstateModel.java
@@ -5,6 +5,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.List;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -19,4 +21,5 @@ public class RealEstateModel {
     private GenderOfTenant genderOfTenant;
     private String firstNameOfTenant;
     private String lastNameOfTenant;
+    private List<ExpenseCategory> listOfExpanseCategories;
 }
