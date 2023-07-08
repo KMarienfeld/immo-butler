5da915f8b8dc2ca18f082cb300fab85276268d6e
Kristina Marienfeld <130476631+KMarienfeld@users.noreply.github.com>
2023-07-08 22:32:41 +0200

    add tenant and address to pdf generator

--- backend/src/main/java/de/neuefische/backend/service/PDFGenerator.java
+++ backend/src/main/java/de/neuefische/backend/service/PDFGenerator.java
@@ -23,78 +23,73 @@ public class PDFGenerator {
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         //PDFWriter mit dem Document und dem ByteArrayOutputStream erzeugen
         PdfWriter writer = PdfWriter.getInstance(document, outputStream);
+        Font font = FontFactory.getFont(FontFactory.Arial, 12);
         //Document öffnen
         document.open();
         //Schrift festlegen
-        Font font = FontFactory.getFont(FontFactory.HELVETICA, 12, Font.BOLD);
-
+        Font fontTitle = FontFactory.getFont(FontFactory.HELVETICA, 12, Font.BOLD);
         //Titel hinzufügen
-        Paragraph title = new Paragraph("Nebenkostenabrechnung " + utilityBillModel.getYear(), font);
-        title.setAlignment(Element.ALIGN_CENTER);
+        Paragraph title = new Paragraph("Nebenkostenabrechnung " + utilityBillModel.getYear(), fontTitle);
+        //Header als Tabelle erstellen
+        PdfPTable headerTable = new PdfPTable(2);
+        headerTable.setWidthPercentage(100);
+        headerTable.getDefaultCell().setBorder(Rectangle.NO_BORDER);
+        addTenantAndAddress(headerTable, utilityBillModel);
+        addLogoToPDF(headerTable);
         //Tabelle erstellen
-        PdfPTable table = new PdfPTable(4);
-        table.setWidthPercentage(70);
-        table.setSpacingBefore(10f);
-        table.setSpacingBefore(10f);
-
+        PdfPTable resultTable = new PdfPTable(4);
+        resultTable.setWidthPercentage(70);
+        resultTable.setSpacingBefore(10f);
+        resultTable.setSpacingBefore(10f);
         //Tabellenüberschrift
-        addTableHeader(table);
+        addTableHeader(resultTable);
         //Tabellenzeilen
-        addTableRows(table, utilityBillModel.getCustomExpenseCategoryModel());
-        addRowWithTotalCosts(table, utilityBillModel.getTotalCostsOfAllExpenseCategories());
-
+        addTableRows(resultTable, utilityBillModel.getCustomExpenseCategoryModel());
+        addRowWithTotalCosts(resultTable, utilityBillModel.getTotalCostsOfAllExpenseCategories());
         //Tabelle zum Document hinzufügen
-        addLogoToPDF(document);
-        addTenantAndAddress(document, utilityBillModel);
+        document.add(headerTable);
         document.add(Chunk.NEWLINE);
         document.add(Chunk.NEWLINE);
         document.add(title);
         document.add(Chunk.NEWLINE);
-        document.add(table);
+        addOpeningWords(document, utilityBillModel);
         document.add(Chunk.NEWLINE);
-
+        document.add(resultTable);
+        document.add(Chunk.NEWLINE);
         //Text hinzufügen
-        addText(document, utilityBillModel);
-        addResult(document, utilityBillModel);
-
+        addPrepayment(document, utilityBillModel);
+        addResult(document, utilityBillModel, font);
+        document.add(Chunk.NEWLINE);
+        addClosingWords(document, utilityBillModel);
+        document.add(Chunk.NEWLINE);
+        document.add(Chunk.NEWLINE);
+        document.add(Chunk.NEWLINE);
+        addSignature(document);
         //Schließe das Document
         document.close();
 
         return outputStream.toByteArray();
     }
 
-    private void addTenantAndAddress(Document document, UtilityBillModel utilityBillModel) throws DocumentException {
-        Paragraph paragraph = new Paragraph();
-        if (utilityBillModel.getGenderOfTenant() == MALE) {
-            paragraph.add("Herr " + utilityBillModel.getFirstNameOfTenant() + " " + utilityBillModel.getLastNameOfTenant());
-        } else {
-            paragraph.add("Frau " + utilityBillModel.getFirstNameOfTenant() + " " + utilityBillModel.getLastNameOfTenant());
-        }
-        paragraph.add(Chunk.NEWLINE);
-        paragraph.add(utilityBillModel.getRoadOfRealEstate() + " " + utilityBillModel.getHouseNumberOfRealEstate());
-        paragraph.add(Chunk.NEWLINE);
-        paragraph.add(utilityBillModel.getPostCodeOfRealEstate() + " " + utilityBillModel.getLocationOfRealEstate());
-        document.add(paragraph);
-    }
-
-    private void addResult(Document document, UtilityBillModel utilityBillModel) throws DocumentException {
+    private void addResult(Document document, UtilityBillModel utilityBillModel, Font font) throws DocumentException {
         Paragraph result = new Paragraph();
+        result.setFont(font);
         if (utilityBillModel.getFinalResult() != 0) {
             double finalResult = utilityBillModel.getFinalResult();
             if (finalResult < 0) {
-                result.add(new Chunk("Daraus ergibt sich eine Rückerstattung in Höhe von " + Math.abs(finalResult) + "€ ."));
+                result.add(new Chunk("Infolgedessen ergibt sich eine Rückerstattung in Höhe von " + Math.abs(finalResult) + "€ ."));
             } else {
-                result.add(new Chunk("Daraus ergibt sich eine Nachzahlung in Höhe von " + finalResult + "€ ."));
+                result.add(new Chunk("Infolgedessen ergibt sich eine Nachzahlung in Höhe von " + finalResult + "€ ."));
             }
         }
         document.add(result);
     }
 
-    private void addText(Document document, UtilityBillModel utilityBillModel) throws DocumentException {
+    private void addPrepayment(Document document, UtilityBillModel utilityBillModel) throws DocumentException {
         Paragraph paragraph = new Paragraph();
-        paragraph.add("Die geleistete Vorauszahlung pro Monat betrug ");
+        paragraph.add("Die monatliche Vorauszahlung in Höhe von ");
         paragraph.add(new Chunk(String.valueOf(utilityBillModel.getPrepaymentMonthly()) + "€ , "));
-        paragraph.add("das entspricht einer Jahreszahlung von ");
+        paragraph.add("summiert sich zu einer jährlichen Zahlung von ");
         paragraph.add(new Chunk(String.valueOf(utilityBillModel.getPrepaymentYear()) + "€ ."));
         document.add(paragraph);
     }
@@ -123,11 +118,67 @@ public class PDFGenerator {
         table.addCell("anteilige Kosten");
     }
 
-    private void addLogoToPDF(Document document) throws DocumentException, IOException {
+    private void addLogoToPDF(PdfPTable table) throws DocumentException, IOException {
         Image logo = Image.getInstance("frontend/src/logo_tuerkis.png");
-        logo.scaleToFit(200, 200);
         logo.setAlignment(Element.ALIGN_RIGHT);
-        document.add(logo);
+        PdfPCell cell = new PdfPCell(logo);
+        cell.setBorder(Rectangle.NO_BORDER);
+        cell.setHorizontalAlignment(Element.ALIGN_RIGHT);
+        cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+        cell.setFixedHeight(100);
+        table.addCell(cell);
+    }
+
+    private void addTenantAndAddress(PdfPTable table, UtilityBillModel utilityBillModel) throws DocumentException {
+        Paragraph paragraph = new Paragraph();
+        if (utilityBillModel.getGenderOfTenant() == MALE) {
+            paragraph.add("Herr " + utilityBillModel.getFirstNameOfTenant() + " " + utilityBillModel.getLastNameOfTenant());
+        } else {
+            paragraph.add("Frau " + utilityBillModel.getFirstNameOfTenant() + " " + utilityBillModel.getLastNameOfTenant());
+        }
+        paragraph.add(Chunk.NEWLINE);
+        paragraph.add(utilityBillModel.getRoadOfRealEstate() + " " + utilityBillModel.getHouseNumberOfRealEstate());
+        paragraph.add(Chunk.NEWLINE);
+        paragraph.add(utilityBillModel.getPostCodeOfRealEstate() + " " + utilityBillModel.getLocationOfRealEstate());
+        PdfPCell cell = new PdfPCell(paragraph);
+        cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+        cell.setBorder(Rectangle.NO_BORDER);
+        table.addCell(cell);
+    }
+
+    private void addOpeningWords(Document document, UtilityBillModel utilityBillModel) throws DocumentException {
+        Paragraph paragraph = new Paragraph();
+        if (utilityBillModel.getGenderOfTenant() == MALE) {
+            paragraph.add("Sehr geehrter Herr " + utilityBillModel.getLastNameOfTenant() + ",");
+        } else {
+            paragraph.add("Sehr geehrte Frau " + utilityBillModel.getLastNameOfTenant() + ",");
+        }
+        paragraph.add(Chunk.NEWLINE);
+        paragraph.add(Chunk.NEWLINE);
+        paragraph.add("mit diesem Schreiben überreiche ich Ihnen die Nebenkostenabrechnung für Ihre Wohneinheit in der " + utilityBillModel.getRoadOfRealEstate() + " " + utilityBillModel.getHouseNumberOfRealEstate() + " in " + utilityBillModel.getPostCodeOfRealEstate() + " " + utilityBillModel.getLocationOfRealEstate() + ", ");
+        paragraph.add("für den Abrechnungszeitraum des Jahres " + utilityBillModel.getYear() + ".");
+        document.add(paragraph);
+    }
+
+    private void addClosingWords(Document document, UtilityBillModel utilityBillModel) throws DocumentException {
+        Paragraph paragraph = new Paragraph();
+        if (utilityBillModel.getFinalResult() != 0) {
+            double finalResult = utilityBillModel.getFinalResult();
+            if (finalResult < 0) {
+                paragraph.add("Der Rückzahlungsbetrag wird Ihnen zeitnah auf Ihr Konto überwiesen.");
+            } else {
+                paragraph.add("Ich bitte um eine zeitnahe Überweisung der ausstehenden Nachzahlung. ");
+            }
+        }
+        paragraph.add(Chunk.NEWLINE);
+        paragraph.add("Ich stehe Ihnen gerne zur Verfügung, falls Sie Fragen haben oder weitere Informationen benötigen.");
+        document.add(paragraph);
+    }
+
+    public void addSignature(Document document) throws DocumentException {
+        Paragraph paragraph = new Paragraph();
+        paragraph.add("Mit freundlichen Grüßen");
+        document.add(paragraph);
     }
 }
 
